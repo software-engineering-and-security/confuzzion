04cf0cc2e08e2b706bc90da0cd07375aa0aa08d1
William Bonnaventure <william.bonnaventure@grenoble-inp.org>
2019-04-09 14:58:35 +0200

    Fix jar path in scripts

--- fuzz-repro.sh
+++ fuzz-repro.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./jqf/bin/jqf-repro -c ./target/confuzzion-1.0-SNAPSHOT-jar-with-dependencies.jar:./target/test-classes/ confuzziontest.ConfuzzionLauncher fuzz $1
+./jqf/bin/jqf-repro -c ./target/confuzzion-1.0-SNAPSHOT-all.jar:./target/test-classes/ confuzziontest.ConfuzzionLauncher fuzz $1
--- fuzz-zest.sh
+++ fuzz-zest.sh
@@ -6,4 +6,4 @@ mkdir -p /dev/shm/fuzz-results
 
 ln -s /dev/shm/fuzz-results ./
 
-./jqf/bin/jqf-ei -c ./target/confuzzion-1.0-SNAPSHOT-jar-with-dependencies.jar:./target/test-classes/:$(./jqf/scripts/classpath.sh) confuzziontest.ConfuzzionLauncher fuzz
+./jqf/bin/jqf-ei -c ./target/confuzzion-1.0-SNAPSHOT-all.jar:./target/test-classes/:$(./jqf/scripts/classpath.sh) -v confuzziontest.ConfuzzionLauncher fuzz
