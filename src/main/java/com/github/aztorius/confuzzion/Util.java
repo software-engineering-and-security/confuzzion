c95b4735c8b8ed5fe1116973a763717c53be3346
William Bonnaventure <william.bonnaventure@grenoble-inp.org>
2019-04-24 09:43:45 +0200

    Add abstractToConcrete method for classes in Util class

--- src/main/java/com/github/aztorius/confuzzion/Util.java
+++ src/main/java/com/github/aztorius/confuzzion/Util.java
@@ -3,7 +3,21 @@ package com.github.aztorius.confuzzion;
 import soot.Local;
 import soot.util.Chain;
 
+import java.util.HashMap;
+
 public class Util {
+    private static HashMap<String, String> childMap;
+
+    static {
+        childMap = new HashMap<String, String>();
+        childMap.put("java.util.concurrent.BlockingQueue",
+                     "java.util.concurrent.ArrayBlockingQueue");
+    }
+
+    public static String abstractToConcrete(String className) {
+        return childMap.get(className);
+    }
+
     public static Local randomLocal(Chain<Local> locals, RandomGenerator rand) {
         if (locals.size() <= 0) {
             return null;
