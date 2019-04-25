a4989dead873a6b189c12126f2f07fa679b01954
William Bonnaventure <william.bonnaventure@grenoble-inp.org>
2019-04-25 16:40:55 +0200

    Add BodyMutation.java

---
+++ src/main/java/com/github/aztorius/confuzzion/BodyMutation.java
@@ -0,0 +1,42 @@
+package com.github.aztorius.confuzzion;
+
+import soot.Body;
+import soot.Local;
+import soot.Unit;
+
+import java.util.ArrayList;
+
+public class BodyMutation {
+    protected Body body;
+    private ArrayList<Local> addedLocals;
+    private ArrayList<Unit> addedUnits;
+    private Unit uReturn;
+
+    public BodyMutation(Body body) {
+        this.body = body;
+        this.addedLocals = new ArrayList<Local>();
+        this.addedUnits = new ArrayList<Unit>();
+        this.uReturn = body.getUnits().getLast();
+    }
+
+    public void undo() {
+        for (Local local : addedLocals) {
+            this.body.getLocals().remove(local);
+        }
+        for (Unit unit : addedUnits) {
+            this.body.getUnits().remove(unit);
+        }
+    }
+
+    public void addLocal(Local local) {
+        this.addedLocals.add(local);
+        this.body.getLocals().add(local);
+    }
+
+    public void addUnit(Unit unit) {
+        this.addedUnits.add(unit);
+        body.getUnits().remove(uReturn);
+        this.body.getUnits().add(unit);
+        body.getUnits().add(uReturn);
+    }
+}
