523e8ad4c5e056d701ade29f4e4983d1f1f158a4
William Bonnaventure <william.bonnaventure@grenoble-inp.org>
2019-05-29 09:18:39 +0200

    Fix typo

--- src/main/java/com/github/aztorius/confuzzion/Status.java
+++ src/main/java/com/github/aztorius/confuzzion/Status.java
@@ -3,7 +3,7 @@ package com.github.aztorius.confuzzion;
 public enum Status {
     SUCCESS, /* Mutation success, and execution return no error */
     FAILED, /* Mutation failed, no execution. */
-    NOTEXECUTED, /* Mutation succeed but the code is not exectued. */
+    NOTEXECUTED, /* Mutation succeed but the code is not executed. */
     CRASHED, /* Mutation success but execution crashed. */
     INTERRUPTED, /* Mutation succeed but the execution has been interrupted. */
     VIOLATES /* Mutation and execution success, but violates a contract. */
