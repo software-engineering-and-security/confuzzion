71dda9d173d644fb05802ace01fe51bae6d94096
William Bonnaventure <william.bonnaventure@grenoble-inp.org>
2019-06-13 17:03:04 +0200

    Add -t -r command line options

--- src/main/java/com/github/aztorius/confuzzion/ConfuzzionMain.java
+++ src/main/java/com/github/aztorius/confuzzion/ConfuzzionMain.java
@@ -11,10 +11,14 @@ import java.util.Timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import soot.Scene;
+
 public class ConfuzzionMain {
     private Path resultFolder;
 
     private static long MAIN_LOOP_ITERATIONS = 1000;
+    private static int TIMEOUT = 1000;
+    private static boolean WITH_JVM = true;
     private static final Logger logger = LoggerFactory.getLogger(ConfuzzionMain.class);
 
     public ConfuzzionMain(Path resultFolder) {
@@ -26,15 +30,29 @@ public class ConfuzzionMain {
             ConfuzzionMain.printHelp();
         } else if (args[0].equals("mut")) {
             long main_loop_iterations = ConfuzzionMain.MAIN_LOOP_ITERATIONS;
+            int timeout = ConfuzzionMain.TIMEOUT;
+            boolean withJVM = ConfuzzionMain.WITH_JVM;
 
             String main_loop_iterations_str =
                 ConfuzzionMain.parseOption(args, "-m");
             if (main_loop_iterations_str != null) {
                 main_loop_iterations = Long.parseLong(main_loop_iterations_str);
             }
+
             Path resultFolder = ConfuzzionMain.parseDirectory(args);
             ConfuzzionMain conf = new ConfuzzionMain(resultFolder);
-            conf.startMutation(main_loop_iterations);
+
+            String timeout_str = ConfuzzionMain.parseOption(args, "-t");
+            if (timeout_str != null) {
+                timeout = Integer.parseInt(timeout_str);
+            }
+
+            String withjvm_str = ConfuzzionMain.parseOption(args, "-r");
+            if (withjvm_str != null) {
+                withJVM = !withjvm_str.equalsIgnoreCase("thread");
+            }
+
+            conf.startMutation(main_loop_iterations, timeout, withJVM);
         } else if (args[0].equals("gen")) {
             long main_loop_iterations = 10;
             String main_loop_iterations_str =
@@ -86,8 +104,9 @@ public class ConfuzzionMain {
 
     private static void printHelp() {
         System.err.println(
-            "Usage: mut [-o directory] [-m MAIN_LOOP_ITERATIONS] [-c CONST_LOOP_ITERATIONS]\n" +
-            "       gen [-m MAIN_LOOP_ITERATIONS]"
+            "Usage: mut [-o directory] [-m MAIN_LOOP_ITERATIONS] [-c CONST_LOOP_ITERATIONS] [-t TIMEOUT_PER_PROGRAM] [-r RUNNER]\n" +
+            "       gen [-m MAIN_LOOP_ITERATIONS]\n" +
+            "RUNNER : thread or jvm"
         );
     }
 
@@ -104,7 +123,11 @@ public class ConfuzzionMain {
         }
     }
 
-    public void startMutation(long mainloop_turn) {
+    public void startMutation(long mainloop_turn, int timeout, boolean withJVM) {
+        Scene.v().loadBasicClasses();
+        Scene.v().extendSootClassPath(Util.getJarPath());
+        logger.info("Soot Class Path: {}", Scene.v().getSootClassPath());
+
         RandomGenerator rand = new RandomGenerator();
         ArrayList<Contract> contracts = new ArrayList<Contract>();
         contracts.add(new ContractTypeConfusion());
@@ -135,10 +158,24 @@ public class ConfuzzionMain {
 
             // Add contracts checks
             ArrayList<BodyMutation> contractsMutations =
-                currentProg.addContractsChecks(contracts, mutation);
+                    currentProg.addContractsChecks(contracts, mutation);
+            // Save current classes to unique folder
+            Path folder = Paths.get(
+                    resultFolder.toAbsolutePath().toString(),
+                    mutation.getClass().getSimpleName() + "-" + loop1);
             try {
                 // Instantiation and launch
-                currentProg.genAndLaunch();
+                if (withJVM) {
+                    try {
+                        Files.createDirectories(folder);
+                    } catch(IOException e2) {
+                        logger.error("Printing last program generated:\n{}", currentProg.toString(), e2);
+                        break;
+                    }
+                    currentProg.genAndLaunchWithJVM(folder.toString(), timeout);
+                } else { //with threads
+                    currentProg.genAndLaunch(timeout);
+                }
                 // Remove contracts checks for next turn
                 currentProg.removeContractsChecks(contractsMutations);
                 // Add mutation to the stack
@@ -149,17 +186,16 @@ public class ConfuzzionMain {
                 logger.warn("Exception while executing program", e);
                 Throwable cause = Util.getCause(e);
                 if (ContractCheckException.class.isInstance(cause)) {
-                    // Save current classes to a unique folder
-                    Path folder = Paths.get(
-                        resultFolder.toString(),
-                        mutation.getClass().getSimpleName() + "-" + loop1);
+                    // Save current classes to unique folder
+                    Path folder2 = Paths.get(
+                            resultFolder.toAbsolutePath().toString(),
+                            mutation.getClass().getSimpleName() + "-" + loop1 + "-ContractCheckException");
                     try {
-                        Files.createDirectories(folder);
+                        Files.createDirectories(folder2);
                     } catch(IOException e2) {
                         logger.error("Printing last program generated:\n{}", currentProg.toString(), e2);
                         break;
                     }
-                    currentProg.saveToFolder(folder.toString());
                     // Update status screen
                     statusScreen.newMutation(mutation.getClass(),
                         Status.VIOLATES, 2);
@@ -176,6 +212,16 @@ public class ConfuzzionMain {
                 currentProg.removeContractsChecks(contractsMutations);
                 // Bad sample, revert mutation
                 mutation.undo();
+            } finally {
+                if (withJVM) {
+                    // Remove folder
+                    try {
+                        Util.deleteDirectory(folder);
+                    } catch(IOException e2) {
+                        logger.error("Error while deleting directory {}", folder, e2);
+                        break;
+                    }
+                }
             }
 
             if (statusScreen.isStalled() && mutationsStack.size() > 0) {
