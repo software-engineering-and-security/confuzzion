4bbe8d40c1e5fa12b507cead386d13113319e2f6
William Bonnaventure <william.bonnaventure@grenoble-inp.org>
2019-06-13 16:37:51 +0200

    Add some useful methods inside Util

--- src/main/java/com/github/aztorius/confuzzion/Util.java
+++ src/main/java/com/github/aztorius/confuzzion/Util.java
@@ -1,9 +1,25 @@
 package com.github.aztorius.confuzzion;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import soot.Scene;
+import soot.SootClass;
 
 public class Util {
     private static HashMap<String, String> childMap;
+    private static final Logger logger = LoggerFactory.getLogger(Util.class);
 
     static {
         childMap = new HashMap<String, String>();
@@ -23,5 +39,60 @@ public class Util {
             result = cause;
         }
         return result;
+    }
+
+    public static SootClass getOrLoadSootClass(String name) {
+        SootClass sootClass = Scene.v().getSootClassUnsafe(name);
+        if (sootClass == null) {
+            sootClass = Scene.v().loadClassAndSupport(name);
+        }
+        return sootClass;
+    }
+
+    public static void deleteDirectory(Path path) throws IOException {
+        if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
+            try (DirectoryStream<Path> entries = Files.newDirectoryStream(path)) {
+                for (Path entry : entries) {
+                    deleteDirectory(entry);
+                }
+            }
+        }
+        Files.delete(path);
+    }
+
+    public static void writeToFile(String fileName, String content) throws IOException {
+        FileOutputStream outputStream = new FileOutputStream(fileName);
+        byte[] strToBytes = content.getBytes();
+        outputStream.write(strToBytes);
+        outputStream.close();
+    }
+
+    public static void startJVM(String classpath, String className, int timeoutMiliseconds) throws Exception {
+        String path = Paths.get(System.getProperty("java.home"), "bin", "java").toString();
+        ProcessBuilder processBuilder = new ProcessBuilder(path, "-cp", classpath + ":" + Util.getJarPath(), className);
+        logger.info("{}", processBuilder.command());
+        Process process = processBuilder.start();
+        process.waitFor(timeoutMiliseconds, TimeUnit.MILLISECONDS);
+        if (process.isAlive()) {
+            process.destroyForcibly();
+            throw new InterruptedException();
+        }
+        //TODO: check if errorCode is a contract check failure code
+        int errorCode = process.exitValue();
+        if (errorCode != 0) {
+            throw new RuntimeException("Error code " + errorCode);
+        }
+    }
+
+    public static String getJarPath() {
+        try {
+            // JAR path
+            String path =
+                    new File(Util.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+            return path;
+        } catch(java.net.URISyntaxException e) {
+            logger.error("Error while getting JAR path", e);
+            return null;
+        }
     }
 }
