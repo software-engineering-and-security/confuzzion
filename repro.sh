daa5b0581b905cc7f66d697aa70cbff1084a7e13
William Bonnaventure <william.bonnaventure@grenoble-inp.org>
2019-06-25 16:38:42 +0200

    Remove java from commande line arguments of Repro

--- repro.sh
+++ repro.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Usage: repro.sh ./path/to/java -i input_file [-s]
+# Usage: repro.sh ./path/to/java -i input_file
 
 LOCAL_JAVA=$1
 
 cd "$(dirname "$0")"
 
-$LOCAL_JAVA -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -cp target/confuzzion-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.aztorius.confuzzion.Repro $@
+$LOCAL_JAVA -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -cp target/confuzzion-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.aztorius.confuzzion.Repro ${@:2}
