4dd7fbc6fd8793f82cc990432863ead0c2a1dea8
William Bonnaventure <william.bonnaventure@grenoble-inp.org>
2019-07-30 16:15:52 +0200

    Update scripts

--- fuzz.sh
+++ fuzz.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-java -Dorg.slf4j.simpleLogger.defaultLogLevel=ERROR -jar ./target/confuzzion-1.0-SNAPSHOT-jar-with-dependencies.jar $@
+# Usage: JAVA=./path/to/java ./fuzz.sh -t target.Class1:target.Class2 [options]
+
+JAVA=${JAVA:=java}
+echo Using: $JAVA
+
+$JAVA -Dorg.slf4j.simpleLogger.defaultLogLevel=ERROR -jar ./target/confuzzion-1.0-SNAPSHOT-jar-with-dependencies.jar $@
--- repro.sh
+++ repro.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# Usage: repro.sh ./path/to/java -i input_file
+# Usage: JAVA=./path/to/java repro.sh -i input_file [options]
 
-LOCAL_JAVA=$1
+JAVA=${JAVA:=java}
+echo Using: $JAVA
 
 cd "$(dirname "$0")"
 
-$LOCAL_JAVA -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -cp target/confuzzion-1.0-SNAPSHOT-jar-with-dependencies.jar confuzzion.Repro ${@:2}
+$JAVA -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -cp target/confuzzion-1.0-SNAPSHOT-jar-with-dependencies.jar confuzzion.Repro ${@:2}
